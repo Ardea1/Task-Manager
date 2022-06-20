116c19b1f15897803cdda9646862af0ca8b243b5
Ardea1 <cmad1@mail.ru>
2022-06-20 22:41:21 +0700

    Learn

--- app/src/main/java/com/example/taskmanager/broadcastReciever/AlarmService.java
+++ app/src/main/java/com/example/taskmanager/broadcastReciever/AlarmService.java
@@ -8,6 +8,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 // IBinder - интерфейс обратного вызова
 // для связи сервиса и приложения.
@@ -26,12 +27,15 @@ import com.example.taskmanager.activity.MainActivity;
 
 public class AlarmService extends Service {
 
-    private int lastId = 0; //постоянно увеличивающееся поле, уникальный номер каждого уведомления
-
     private static final int NOTIFICATION_ID = 3;
 
     public AlarmService(String name) {
         super();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
     }
 
     @Override
@@ -49,21 +53,21 @@ public class AlarmService extends Service {
                     NotificationManager.IMPORTANCE_HIGH);
             channel.setDescription("My channel description");
             channel.enableLights(true);
-            channel.enableVibration(false);
+            channel.enableVibration(true);
             mNotificationManager.createNotificationChannel(channel);
         }
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "YOUR_CHANNEL_ID")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setTicker("Новое уведомление")
+                .setWhen(System.currentTimeMillis())
                 .setSmallIcon(R.mipmap.ic_launcher) // значок уведомления
                 .setContentTitle("Уведомление") // заголовок для уведомления
                 .setContentText("Будильник")// сообщение для уведомления
-                .setAutoCancel(false); // очистить уведомление после клика
+                .setAutoCancel(true); // очистить уведомление после клика
         Intent i = new Intent(getApplicationContext(), MainActivity.class);
         PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
         mBuilder.setContentIntent(pi);
-        mNotificationManager.notify(lastId, mBuilder.build());
-
-
-        lastId ++;
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 
     @Nullable
@@ -71,4 +75,5 @@ public class AlarmService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
 }
